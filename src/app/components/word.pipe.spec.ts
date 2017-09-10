2df0368ab769dcbc0851f3cf8b8e113e5ae84a8a
Ben <benl@b2sign.com>
2017-09-09 23:22:37 -0700

    remove word.pipe

--- src/app/components/word.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { WordPipe } from './word.pipe';
-
-describe('WordPipe', () => {
-  it('create an instance', () => {
-    const pipe = new WordPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
